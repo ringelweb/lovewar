ba49b8507fc6a241b76e18cdf1893e78fdb32be8
John Doe <bob@example.com>
2018-01-15 13:13:43 +0530

    dbchanged

--- src/main/java/org/inayat/novo/ringelweb/database/DBCredentials.java
+++ src/main/java/org/inayat/novo/ringelweb/database/DBCredentials.java
@@ -1,9 +1,14 @@
 package org.inayat.novo.ringelweb.database;
 
 public interface DBCredentials {
-	
+	/*
 	    String DBURL = "jdbc:mysql://localhost:3306/loversdb";
 	    String DRIVER = "com.mysql.jdbc.Driver";
 	    String USER = "root";
-	    String PASSWORD = "";
+	    String PASSWORD = "";*/
+	
+	    String DBURL = "jdbc:mysql://databases.000webhost.com/id3681939_loversdb";
+	    String DRIVER = "com.mysql.jdbc.Driver";
+	    String USER = "id3681939_lover";
+	    String PASSWORD = "lover";
 }
